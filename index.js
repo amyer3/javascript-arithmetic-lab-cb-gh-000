0380115b19b04d3606f7ed40af15ea81c0c0ee68
Alexander Myers <alexander.j.myers@icloud.com>
2019-04-16 21:34:52 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function add(a, b){return a+b}
+function subtract(a, b){return a-b}
+function multiply(a, b){return a*b}
+function divide(a, b){return a/b}
+function inc(a){return ++a}
+function dev(a){return --a}
+function makeInt(a){
+  
+}
